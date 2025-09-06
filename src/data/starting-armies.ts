b549633907e9f7138cf6b2d867624b9d9ffe8b91
Gabor Bajo <gabor_bajo@epam.com>
2025-09-06 11:19:26 +0200

    refine retreat animation

--- src/data/starting-armies.ts
+++ src/data/starting-armies.ts
@@ -11,13 +11,13 @@ export const STARTING_ARMIES: Army[] = [
     id: 'army1',
     owner: 'ott',
     strength: 30,
-    position: 'c1',
+    position: 'f2',
     positionHistory: ['f2', 'f1', 'c1', 'c2', 'f7'],
   },
   {
     id: 'army2',
     owner: 'hun',
-    strength: 30,
+    strength: 15,
     position: 'f3',
     positionHistory: ['f3', 'f4', 'f5', 'f6', 'f11'],
   }
